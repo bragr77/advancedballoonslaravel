52307bfed5a09155aece833f46cd9ec3ef371b06
Raul Gonzalez <bragr77@gmail.com>
2021-07-03 19:05:10 -0400

    configuración para que los mails se envíen de manera asíncrona en segundo plano

--- app/Mail/MensajeRecibido.php
+++ app/Mail/MensajeRecibido.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class MensajeRecibido extends Mailable
+class MensajeRecibido extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- app/Mail/RespuestaAutomatica.php
+++ app/Mail/RespuestaAutomatica.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class RespuestaAutomatica extends Mailable
+class RespuestaAutomatica extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
